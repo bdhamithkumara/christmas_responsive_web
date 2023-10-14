c997f74e84a24b6ae7d4ee518cd221a084696327
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-10-14 13:24:59 +0530

    completed giving section

--- app/layout/GivingSection.jsx
+++ app/layout/GivingSection.jsx
@@ -1,9 +1,59 @@
 "use client"
 import React from 'react'
+import {GivingSectionImage1, GivingSectionImage2, GivingSectionImage3} from '../../public/images/index'
+import Image from 'next/image'
+
+const GivingSectionData = [
+  {
+    id : "1",
+    image : GivingSectionImage1,
+    altText : "Giving Section Image 1",
+    title : "Surprise gifts",
+    description : "They are the best gifts there is.",
+  },
+  {
+    id : "2",
+    image : GivingSectionImage2,
+    altText : "Giving Section Image 2",
+    title : "Ornaments",
+    description : "Give a moment to decorate",
+  },
+  {
+    id : "3",
+    image : GivingSectionImage3,
+    altText : "Giving Section Image 3",
+    title : "Lots of love",
+    description : "Give away feelings that last forever",
+  }
+];
+
 
 const GivingSection = () => {
   return (
-    <div>GivingSection</div>
+    <div>
+      <div className='flex justify-center mt-24 flex-col items-center'>
+        <div>
+          <p className='text-[24px] w-[200px] font-semibold text-center text-title-color dark:text-text-color-dark'>
+          Start Giving This Christmas
+          </p>
+        </div>
+        <div className='grid lg:mt-16 mt-8 grid-cols-2 lg:grid-cols-3'>
+          {GivingSectionData.map((Giving_Section_Data) => (
+            <div key={Giving_Section_Data.id} className='text-center items-center flex justify-center flex-col lg:mx-12 md:mx-8 mx-5 cursor-pointer'>
+            <div>
+              <Image src={Giving_Section_Data.image} alt={Giving_Section_Data.altText} className='xs:w-[120px] xs:h-[120px] w-[100px] h-[100px] m-5'/>
+            </div>
+            <div>
+              <p className='text-[16px] xs:text-[16px] font-semibold dark:text-title-color-dark text-title-color'>{Giving_Section_Data.title}</p>
+            </div>
+            <div>
+              <p className='text-[13px] xs:text-[14px] w-[150px] font-normal text-text-color dark:text-text-color-dark mt-1'>{Giving_Section_Data.description}</p>
+            </div>
+          </div>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- app/layout/HeroSection.jsx
+++ app/layout/HeroSection.jsx
@@ -1,9 +1,28 @@
 "use client"
 import React from 'react'
+import {HeroImage} from '../../public/images/index'
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <div>
+      <div className='flex justify-center lg:flex-row flex-col items-center text-center lg:text-start sm:mt-8 md:mt-20'>
+        <div>
+          <Image src={HeroImage} alt='heroImage' className='xs:w-[500px] xs:h-[442px] w-[320px] h-[282px]'/>
+        </div>
+        <div className='my-auto'>
+          <div className='justify-center flex lg:justify-start'>
+            <p className='text-[24px] xs:text-[42px] font-semibold xs:w-[500px] w-[300px] leading-tight'>Merry Christmas and Happy New Year!</p>
+          </div>
+          <div className='justify-center flex lg:justify-start'>
+            <p className='text-[13px] sm:text-[15px] w-[280px] sm:w-[380px] font-normal text-text-color mt-5 dark:text-text-color-dark'>Christmas and a new year is about to begin, all good wishes and successes.</p>
+          </div>
+          <div className='flex justify-center items-center lg:justify-start'>
+            <div className='rounded-[64px] w-fit h-auto bg-first-color px-12 py-5 text-[15px] xs:text-[16px] font-medium text-white mt-10 cursor-pointer'>Get Started</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

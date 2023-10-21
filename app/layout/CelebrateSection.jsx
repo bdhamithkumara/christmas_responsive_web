0b6e2460c3328e258f3b4af1c9ebc28b2ace4cdc
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-10-21 12:29:22 +0530

    completed gift section

--- app/layout/CelebrateSection.jsx
+++ app/layout/CelebrateSection.jsx
@@ -1,9 +1,28 @@
 "use client"
 import React from 'react'
+import {CelebrateSectionImage} from '../../public/images/index'
+import Image from 'next/image'
 
 const CelebrateSection = () => {
   return (
-    <div>CelebrateSection</div>
+    <div className='flex justify-center lg:flex-row flex-col items-center text-center lg:text-start mt-16 xs:mt-20 lg:mt-10'>
+      <div className='my-auto sm:w-[400px]'>
+        <div className='justify-center lg:justify-start flex'>
+          <p className='text-[20px] xs:text-[24px] font-semibold xs:w-[220px] w-[200px] leading-tight text-title-color dark:text-title-color-dark'>Celebrate With A Lot Of Love</p>
+        </div>
+        <div className='flex justify-center lg:justify-start'>
+          <p className='text-[15px] xs:text-[16px] w-[315px] xs:w-[330px] lg:w-[350px] font-normal text-text-color mt-5 dark:text-text-color-dark'>In this holidays, celebrate with much love and peace, offering many blessings to our loved ones, friends and neighbors, wishing them a good Christmas message.</p>
+        </div>
+        <div className='flex justify-center items-center lg:justify-start'>
+          <div className='rounded-[64px] w-fit h-auto bg-first-color px-12 py-4 text-[15px] xs:text-[16px] font-medium text-white mt-10 cursor-pointer'>
+          Send Good Wishes
+          </div>
+        </div>
+      </div>
+      <div>
+        <Image src={CelebrateSectionImage} alt='CelebrateSection Image' className='xs:w-[450px] xs:h-[450px] w-[280px] h-[280px]' />
+      </div>
+    </div>
   )
 }
 
--- app/layout/GiftSection.jsx
+++ app/layout/GiftSection.jsx
@@ -1,9 +1,97 @@
 "use client"
-import React from 'react'
+import React , {useState} from 'react'
+import {GiftSectionImage1,GiftSectionImage2,GiftSectionImage3,GiftSectionImage4,GiftSectionImage5} from '../../public/images/index'
+import Image from 'next/image'
+import {BsHeart , BsHeartFill} from 'react-icons/bs'
+
+const GiftSectionData = [
+
+  {
+    id : 1,
+    image: GiftSectionImage1,
+    altText : "GiftSection Image 1 ",
+    price : "$15",
+    title : "Gingerbread",
+  },
+  {
+    id : 2,
+    image: GiftSectionImage2,
+    altText : "GiftSection Image 2 ",
+    price : "$22",
+    title : "Santa Claus Hat",
+  },
+  {
+    id : 3,
+    image: GiftSectionImage3,
+    altText : "GiftSection Image 3 ",
+    price : "$48",
+    title : "Christmas Tree",
+  },
+  {
+    id : 4,
+    image: GiftSectionImage4,
+    altText : "GiftSection Image 4 ",
+    price : "$35",
+    title : "Snowman",
+  },
+  {
+    id : 5,
+    image: GiftSectionImage5,
+    altText : "GiftSection Image 5 ",
+    price : "$12",
+    title : "Candy Stick",
+  },
+
+];
 
 const GiftSection = () => {
+
+  const [fav , setFav] = useState(Array(GiftSectionData.length).fill(false));
+
+  const handleClick = (index) => {
+    const newFav = [...fav];
+    newFav[index] = !newFav[index]
+    setFav(newFav);
+  }
+
   return (
-    <div>GiftSection</div>
+    <div className='flex justify-center mt-24 flex-col items-center mb-5'>
+      <div>
+        <p className='text-[24px] w-[200px] font-semibold text-center text-title-color dark:text-title-color-dark'>Share A Gift</p>
+      </div>
+      <div className='grid lg:mt-8 mt-4 grid-cols-2 lg:grid-cols-3'>
+        {GiftSectionData.map((Gift_Section_Data,index) => (
+
+          <div key={Gift_Section_Data.id} className='items-center flex flex-col xs:mx-4 xs:my-4 my-5 mx-5 xs:w-[200px] xs:h-[247px] w-[144px] h-[192px] rounded-xl shadow-lg bg-container-color dark:bg-container-color-dark'>
+          <div className='mt-4'>
+            <div onClick={() => handleClick(index)}>
+              {fav[index] ? 
+                (<BsHeartFill className='w-[20px] h-[20px] text-first-color float-right absolute xs:ml-[60px] xs:mt-[5px] ml-[40px] mt-[1px]'/>)
+                :
+                (<BsHeart className='w-[20px] h-[20px] text-first-color float-right absolute xs:ml-[60px] xs:mt-[5px] ml-[40px] mt-[1px]'/>)
+              }
+            </div>
+          </div>
+          <div>
+            <Image src={Gift_Section_Data.image} alt={Gift_Section_Data.altText} className='xs:w-[120px] xs:h-[120px] w-[100px] h-[100px] m-5' />
+          </div>
+          <div className='xs:w-[160px] w-[120px] xs:mt-[-10px] mt-[-28px]'>
+            <div>
+              <p className='text-[16px] xs:text-[20px] font-semibold dark:text-title-color-dark text-title-color'>
+                {Gift_Section_Data.price}
+              </p>
+            </div>
+            <div>
+              <p className='text-[13px] xs:text-[14px] font-normal text-text-color dark:text-text-color-dark mt-1'>
+                {Gift_Section_Data.title}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        ))}
+      </div>
+    </div>
   )
 }
 
